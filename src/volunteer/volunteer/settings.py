5306eba757c648c9e21cf7284f56ea2d8d7ac610
EngCoder-Hawraa <h.a.j.engineer@gmail.com>
2021-10-26 15:04:48 +0300

    volunteer for IRAQ

--- src/volunteer/volunteer/settings.py
+++ src/volunteer/volunteer/settings.py
@@ -175,7 +175,7 @@ EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'testhawraa9@gmail.com'
-EMAIL_HOST_PASSWORD = '######'
+EMAIL_HOST_PASSWORD = 'Test12345678'
 
 AUTH_USER_MODEL="iraq.CustomUser"
 AUTHENTICATION_BACKENDS=['iraq.EmailBackEnd.EmailBackEnd']
